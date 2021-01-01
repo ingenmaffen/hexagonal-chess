d6f202aa1b68b293fa3f351e8776b377733dc934
Rabi Róbert <ingenmaffen@gmail.com>
2021-01-01 05:34:58 +0100

    Restructure code, add dark mode and some comments

--- game.js
+++ game.js
@@ -1,39 +1,40 @@
+// initiate canvas
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 canvas.style.width = window.innerHeight;
 canvas.style.height = window.innerHeight;
-const gameScale = window.innerHeight / 1080;
-canvas.addEventListener("click", (event) => {
-  const x = event.layerX / gameScale;
-  const y = event.layerY / gameScale;
-  const offsetX = hexagonWidth / 2;
-  const offsetY = hexagonHeight / 2;
-  const possibleClicks = [];
-  for (const [key, value] of Object.entries(hexagons)) {
-    if (
-      value.x + offsetX < x &&
-      value.y + offsetY < y &&
-      value.x + offsetX + hexagonWidth - scale / 2 > x &&
-      value.y + offsetY + hexagonHeight > y
-    ) {
-      possibleClicks.push(key);
-    }
-  }
-  if (possibleClicks.length === 1) {
-    console.log(possibleClicks[0]);
-  }
-});
-
 window.addEventListener("resize", (_event) => {
   canvas.style.width = window.innerHeight;
   canvas.style.height = window.innerHeight;
   gameScale = window.innerHeight / 1080;
 });
 
+// variable definitions
+let gameScale = window.innerHeight / 1080;
 const scale = 50;
 const hexagonHeight = scale * Math.cos(Math.PI / 6) * 2;
 const hexagonWidth = 1.5 * scale;
+const hexagons = {};
+const gameState = {
+  white: {
+    pawn: ["b1", "c2", "d3", "e4", "f5", "g4", "h3", "i2", "k1"],
+    bishop: ["f1", "f2", "f3"],
+    rook: ["c1", "i1"],
+    knight: ["d1", "h1"],
+    queen: ["e1"],
+    king: ["g1"],
+  },
+  black: {
+    pawn: ["b7", "c7", "d7", "e7", "f7", "g7", "h7", "i7", "k7"],
+    bishop: ["f11", "f10", "f9"],
+    rook: ["c8", "i8"],
+    knight: ["d9", "h9"],
+    queen: ["e10"],
+    king: ["g10"],
+  },
+};
 
+// drawing methods
 const drawHexagon = (relativeX = 0, relativeY = 0, color = "#ff0000") => {
   ctx.fillStyle = color;
   relativeX += hexagonWidth / 2;
@@ -49,9 +50,10 @@ const drawHexagon = (relativeX = 0, relativeY = 0, color = "#ff0000") => {
   ctx.fill();
 };
 
-const hexagons = {};
 const drawBoard = () => {
-  const colors = ["#D28C45", "#E9AD70", "#FFCF9F"];
+  const colorsLight = ["#D28C45", "#E9AD70", "#FFCF9F"];
+  const colorsDark = ["#001540", "#001C57", "#00316E"];
+  const colors = dark ? colorsDark : colorsLight;
   const fieldDefinition = {
     a: 6,
     b: 7,
@@ -82,55 +84,99 @@ const drawBoard = () => {
   }
 };
 
-drawBoard();
+const drawGameState = () => {
+  for (const [key, value] of Object.entries(gameState.white)) {
+    value.forEach((field) => {
+      const img = new Image();
+      img.src = `./assets/${key}-white.svg`;
+      img.onload = () => {
+        ctx.drawImage(
+          img,
+          hexagons[field].x + hexagonWidth * 0.45,
+          hexagons[field].y + hexagonHeight * 0.65,
+          hexagonWidth * 0.75,
+          hexagonHeight * 0.75
+        );
+      };
+    });
+  }
 
-const gameState = {
-  white: {
-    pawn: ["b1", "c2", "d3", "e4", "f5", "g4", "h3", "i2", "k1"],
-    bishop: ["f1", "f2", "f3"],
-    rook: ["c1", "i1"],
-    knight: ["d1", "h1"],
-    queen: ["e1"],
-    king: ["g1"],
-  },
-  black: {
-    pawn: ["b7", "c7", "d7", "e7", "f7", "g7", "h7", "i7", "k7"],
-    bishop: ["f11", "f10", "f9"],
-    rook: ["c8", "i8"],
-    knight: ["d9", "h9"],
-    queen: ["e10"],
-    king: ["g10"],
-  },
+  for (const [key, value] of Object.entries(gameState.black)) {
+    value.forEach((field) => {
+      const img = new Image();
+      img.src = `./assets/${key}-black.svg`;
+      img.onload = () => {
+        ctx.drawImage(
+          img,
+          hexagons[field].x + hexagonWidth * 0.45,
+          hexagons[field].y + hexagonHeight * 0.65,
+          hexagonWidth * 0.75,
+          hexagonHeight * 0.75
+        );
+      };
+    });
+  }
 };
 
-for (const [key, value] of Object.entries(gameState.white)) {
-  value.forEach((field) => {
-    const img = new Image();
-    img.src = `./assets/${key}-white.svg`;
-    img.onload = () => {
-      ctx.drawImage(
-        img,
-        hexagons[field].x + hexagonWidth * 0.45,
-        hexagons[field].y + hexagonHeight * 0.65,
-        hexagonWidth * 0.75,
-        hexagonHeight * 0.75
-      );
-    };
-  });
+// dark/light mode
+let dark = localStorage.getItem("darkMode");
+dark = dark && dark === "true";
+if (dark) {
+  document.body.style.backgroundColor = "black";
 }
 
-for (const [key, value] of Object.entries(gameState.black)) {
-  value.forEach((field) => {
-    const img = new Image();
-    img.src = `./assets/${key}-black.svg`;
-    img.onload = () => {
-      ctx.drawImage(
-        img,
-        hexagons[field].x + hexagonWidth * 0.45,
-        hexagons[field].y + hexagonHeight * 0.65,
-        hexagonWidth * 0.75,
-        hexagonHeight * 0.75
-      );
-    };
-  });
-}
+const appendDarkLightButton = () => {
+  const button = document.createElement("BUTTON");
+  button.innerHTML = `<img width="32" height="32" src="assets/${
+    dark ? "sun" : "moon"
+  }.svg" />`;
+  button.style.marginRight = 10;
+  button.style.marginTop = 10;
+  button.style.backgroundColor = "transparent";
+  button.style.border = "none";
+  button.onclick = () => {
+    switchDarkLightMode();
+    document.getElementById("button-container").removeChild(button);
+    appendDarkLightButton();
+  };
+  document.getElementById("button-container").append(button);
+};
+
+const switchDarkLightMode = () => {
+  dark = !dark;
+  if (dark) {
+    document.body.style.backgroundColor = "black";
+  } else {
+    document.body.style.backgroundColor = "white";
+  }
+  localStorage.setItem("darkMode", dark);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  drawGameState();
+  drawBoard();
+};
+
+// handle game events
+canvas.addEventListener("click", (event) => {
+  const x = event.layerX / gameScale;
+  const y = event.layerY / gameScale;
+  const offsetX = hexagonWidth / 2;
+  const offsetY = hexagonHeight / 2;
+  const possibleClicks = [];
+  for (const [key, value] of Object.entries(hexagons)) {
+    if (
+      value.x + offsetX < x &&
+      value.y + offsetY < y &&
+      value.x + offsetX + hexagonWidth - scale / 2 > x &&
+      value.y + offsetY + hexagonHeight > y
+    ) {
+      possibleClicks.push(key);
+    }
+  }
+  if (possibleClicks.length === 1) {
+    console.log(possibleClicks[0]);
+  }
+});
+
+drawBoard();
+drawGameState();
+appendDarkLightButton();
